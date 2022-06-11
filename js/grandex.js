eeaeb65464ba258eddd144d5756a9ffe401ca012
erikwski <ferrarinumero10@gmail.com>
2022-06-11 20:49:44 +0200

    sistemato link a video

--- js/grandex.js
+++ js/grandex.js
@@ -232,16 +232,12 @@ $(function () {
 
     //comunicazione
     $("#section6").append(`
-<<<<<<< HEAD
-      <div id="video_yt_7" class="youtube_video" data-property="{videoURL:'https://youtu.be/hfN6Ogc5hDM',containment:'body',autoPlay:true, mute:true, startAt:0, opacity:1}" style="position: absolute;left: 46.8%;width: 28.5%;top: 64.4%;height: 28.5%;"></div>
-=======
-      <div id="video_yt_7" class="youtube player" data-property="{videoURL:'https://youtu.be/hfN6Ogc5hDM',containment:'body',autoPlay:true, mute:true, startAt:0, opacity:1}" style="position: absolute;left: 46.8%;width: 28.5%;top: 64.4%;height: 28.5%;">
+      <div id="video_yt_7" class="youtube_video" data-property="{videoURL:'https://youtu.be/hfN6Ogc5hDM',containment:'body',autoPlay:true, mute:true, startAt:0, opacity:1}" style="position: absolute;left: 46.8%;width: 28.5%;top: 64.4%;height: 28.5%;">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-circle" viewBox="0 0 16 16">
           <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
           <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
         </svg>
       </div>
->>>>>>> 0a70f01b9b187e3a2aae7ed4e7a654a4abbbadbb
     `);
     $("#video_yt_7").click(() => {
       console.log("video_yt_7");
